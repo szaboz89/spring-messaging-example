4a87b96dd748b829b5c42f9d1e2a4ac8bb525703
Zoltán Szabó <szaboz89@gmail.com>
2019-11-05 15:20:46 +0100

    Initial commit

---
+++ src/main/java/com/szabodev/spring/messaging/example/SpringMessagingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.szabodev.spring.messaging.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMessagingExampleApplication.class, args);
+    }
+
+}
